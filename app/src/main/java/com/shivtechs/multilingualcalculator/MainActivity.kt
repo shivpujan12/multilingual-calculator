2af1ede996a9bd58a03738aeeb54477aa7dd0874
shivpujan12 <shivpujanyadav12@gmail.com>
2022-06-23 16:07:45 +0530

    Test commit 2
    Signed-off-by: shivpujan12 <shivpujanyadav1212@gmail.com>

--- app/src/main/java/com/shivtechs/multilingualcalculator/MainActivity.kt
+++ app/src/main/java/com/shivtechs/multilingualcalculator/MainActivity.kt
@@ -1,21 +1,17 @@
 package com.shivtechs.multilingualcalculator
 
 import android.content.Context
-import android.content.DialogInterface
-import android.os.Bundle
-import android.support.v7.app.AlertDialog
-import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.view.WindowManager
-import java.util.*
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,7 +26,6 @@ class MainActivity : AppCompatActivity() {
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(LocaleHelper.onAttach(base))
     }
-//Hello this is shivpujan here
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
